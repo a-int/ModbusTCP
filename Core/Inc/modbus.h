8f0d2d71e76c4cd7a4d8377cf018bf4f5753a028
a-int <somemailforicloud@icloud.com>
2024-02-20 14:21:56 +0300

    add processing for read commands and errors

--- Core/Inc/modbus.h
+++ Core/Inc/modbus.h
@@ -1,8 +1,51 @@
 #ifndef INC_MODBUS_H_
 #define INC_MODBUS_H_
+#include "inttypes.h"
+
+#define MB_TCP_PORT (502)
 
 #define MB_PDU_MAXSIZE (253)
 #define MB_MBAP_SIZE (7)
 #define MB_ADU_MAXSIZE (MB_PDU_MAXSIZE + MB_MBAP_SIZE)
-#define MB_TCP_PORT (502)
+
+#define MB_MBAP_TRANSACTION_ID_H	(0)
+#define MB_MBAP_TRANSACTION_ID_L	(1)
+#define MB_MBAP_PROTOCOL_ID_H 		(2)
+#define MB_MBAP_PROTOCOL_ID_L 		(3)
+#define MB_MBAP_LEN_H 						(4)
+#define MB_MBAP_LEN_L 						(5)
+#define MB_MBAP_CLIENT_ID 				(6)
+
+#define MB_PDU_FN									(7)
+//req byte positions
+#define MB_PDU_ST_ADDR_H					(8)
+#define MB_PDU_ST_ADDR_L					(9)
+#define MB_PDU_QUANTITY_H					(10)
+#define MB_PDU_QUANTITY_L					(11)
+// repl byte positions
+#define MB_PDU_VAL_START					(MB_PDU_FN+1)
+#define MB_PDU_EXCEPTION_CODE			(MB_PDU_VAL_START)
+
+
+#define MB_COILS_ST			(0)
+#define MB_COILS_Q			(4)
+#define MB_DISCRETE_ST	(MB_COILS_ST + MB_COILS_Q)
+#define MB_DISCRETE_Q		(0)
+#define MB_HOLDING_ST 	(MB_DISCRETE_ST + MB_DISCRETE_Q)
+#define MB_HOLDING_Q		(0 * 2)
+#define MB_INPUT_ST			(MB_HOLDING_ST + MB_HOLDING_Q)
+#define MB_INPUT_Q			(4 * 2)
+#define MB_MEMORY_SIZE	(MB_COILS_Q + MB_DISCRETE_Q + MB_HOLDING_Q + MB_INPUT_Q)
+
+#define MB_FN_READ_COILS		(0x01)
+#define MB_FN_READ_DISCRETE	(0x02)
+#define MB_FN_READ_HOLDING	(0x03)
+#define MB_FN_READ_INPUT		(0x04)
+
+#define MB_EXCEPTION_OK					(0x00)
+#define MB_EXCEPTION_FN_UNSUPPORTED (0x01)
+#define MB_EXCEPTION_DATA_ADDR			(0x02)
+#define MB_EXCEPTION_DATA_VAL				(0x03)
+
+uint16_t mb_process(char *mb_repl_buf, char *mb_req_buf, uint16_t req_buf_len);
 #endif /* INC_MODBUS_H_ */
--- Core/Src/tcpserver.c
+++ Core/Src/tcpserver.c
@@ -1,57 +1,37 @@
 #include "lwip/opt.h"
-
 #include "lwip/api.h"
 #include "lwip/sys.h"
-
-#include "tcpserver.h"
 #include "string.h"
 
+#include "tcpserver.h"
 #include "modbus.h"
 
 static struct netconn *conn, *newconn;
 static struct netbuf *netbuf;
 char mb_req_buf[MB_ADU_MAXSIZE];
+char mb_repl_buf[MB_ADU_MAXSIZE];
 
-void copyString(char* dest, char* src, unsigned num);
+static void copyString(char* dest, char* src, unsigned num);
 
 static void tcp_thread(void *arg) {
 	err_t err, accept_err;
-
-	/* Create a new connection identifier. */
 	conn = netconn_new(NETCONN_TCP);
-
 	if (conn != NULL) {
-		/* Bind connection to the modbus tcp port . */
 		err = netconn_bind(conn, IP_ADDR_ANY, MB_TCP_PORT);
-
 		if (err == ERR_OK) {
-			/* Tell connection to go into listening mode. */
 			netconn_listen(conn);
-
 			while (1) {
-				/* Grab new connection. */
 				accept_err = netconn_accept(conn, &newconn);
-
-				/* Process the new connection. */
 				if (accept_err == ERR_OK) {
-
-					/* receive the data from the client */
 					while (netconn_recv(newconn, &netbuf) == ERR_OK) {
-
-						/* If there is some data remaining to be sent, the following process will continue */
 						do {
-							copyString(mb_req_buf, (netbuf->p->payload), netbuf->p->len); // get the message from the client
-
-							u16_t mb_req_len = netbuf->p->len;
-							//mb_process(mb_req_buf, mb_req_len);
-
-							netconn_write(newconn, mb_req_buf, netbuf->p->len, NETCONN_COPY); // send the message back to the client
+							copyString(mb_req_buf, netbuf->p->payload, netbuf->p->len); // get the message from the client
+							uint16_t buf_len = netbuf->p->len;
+							buf_len = mb_process(mb_repl_buf, mb_req_buf, buf_len); // process the data
+							netconn_write(newconn, mb_repl_buf, buf_len, NETCONN_COPY); // send the message back to the client
 						} while (netbuf_next(netbuf) > 0);
-
 						netbuf_delete(netbuf);
 					}
-
-					/* Close connection and discard connection identifier. */
 					netconn_close(newconn);
 					netconn_delete(newconn);
 				}
@@ -63,11 +43,11 @@ static void tcp_thread(void *arg) {
 }
 
 void tcpserver_init(void) {
-	sys_thread_new("tcp_thread", tcp_thread, NULL, DEFAULT_THREAD_STACKSIZE,
+	sys_thread_new("tcp_thread", tcp_thread, NULL, 8*DEFAULT_THREAD_STACKSIZE,
 			osPriorityNormal);
 }
 
-void copyString(char* dest, char* src, unsigned num){
+static void copyString(char* dest, char* src, unsigned num){
 	for(unsigned i = 0; i != num; ++i){
 		dest[i] = src[i];
 	}
